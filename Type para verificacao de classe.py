a15adb766a389f10403eebcf94acdb6e8efb005b
Meira <rodolfo.meira.rm@gmail.com>
2024-10-17 23:55:57 -0300

    first commit

---
+++ Type para verificacao de classe.py
@@ -0,0 +1,15 @@
+# Teste estes códigos no ambiente interativo Idle
+
+inteiro = 2
+print(type(inteiro))
+
+real = 36.9
+print(type(real))
+
+res = inteiro * real
+print(type(res))
+
+msg = 'Total ='
+print(type(msg))
+
+print(msg, res)
---
+++ mensagem com resultado.py
@@ -0,0 +1,5 @@
+inteiro = 2
+real = 36.90
+res = inteiro * real
+msg = 'Total ='
+print(msg, res)
